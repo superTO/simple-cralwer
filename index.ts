ba37922ae688d31e243d6277062727b6106f016e
superTO <to840116@gmail.com>
2021-11-29 22:48:11 +0800

    sol 2

--- index.ts
+++ index.ts
@@ -1,40 +1,44 @@
 import * as pptr from 'puppeteer';
 
-// (async () => {
-//     const browser = await pptr.launch({headless: false});
-//     // const browser = await pptr.launch();
-
-//     const page = await browser.newPage();
-
-//     await page.goto('https://market.binaryx.pro/#/');
-//     await page.waitForSelector('a')
-//     let body = await page.content();
-//     console.log(body);
-
-//     // await page.goto('https://market.binaryx.pro/getSales?page=1&page_size=5&status=selling&sort=time&direction=desc');
-
-//     // await page.reload();
-
-//     // const res = await page.waitForResponse('https://market.binaryx.pro/getSales?page=1&page_size=5&status=selling&sort=time&direction=desc')
-//     // console.log(res.ok());
-//     // console.log(res);
-//     // console.log(await res.json());
-
-//     // setTimeout(async () => {
-//     //     await browser.close();
-//     // }, 2000);
-//     await browser.close();
-// })();
-
-// Sol 1: need "await pptr.launch({headless: false})"
 (async () => {
-    const browser = await pptr.launch({headless: false});
+    // const browser = await pptr.launch({headless: false});
+    const browser = await pptr.launch();
 
     const page = await browser.newPage();
-    const apiContent = await page.goto('https://market.binaryx.pro/getSales?page=1&page_size=5&status=selling&sort=time&direction=desc');
-    console.log(apiContent.ok());
-    console.log(await apiContent.text());
-    console.log(await apiContent.json());
+
+    page.on('response', async response => {
+        if(response.url().includes('getSales?page=1&page_size=5&status=selling&sort=time&direction=desc')){
+            // console.log(response.request());
+            console.log(await response.text());
+        }
+    })
+
+    await page.setExtraHTTPHeaders({
+        'sec-ch-ua': '"Chromium";v="93", " Not;A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.0 Safari/537.36'
+    })
+    await page.goto('https://market.binaryx.pro/getSales?page=1&page_size=5&status=selling&sort=time&direction=desc');
+
+    await page.waitForTimeout(1000);
+    // await browser.waitForTarget(x => x.url() === 'https://market.binaryx.pro/getSales?page=1&page_size=5&status=selling&sort=time&direction=desc')
+    
+    // await page.waitForResponse(res => res.ok());
 
     await browser.close();
 })();
+
+// Sol 1: need "await pptr.launch({headless: false})"
+// (async () => {
+//     const browser = await pptr.launch({headless: false});
+
+//     const page = await browser.newPage();
+//     const apiContent = await page.goto('https://market.binaryx.pro/getSales?page=1&page_size=5&status=selling&sort=time&direction=desc');
+//     console.log(apiContent.ok());
+//     console.log(await apiContent.text());
+//     console.log(await apiContent.json());
+
+//     await browser.close();
+// })();
